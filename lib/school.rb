51f1595e005792dfecd5819bb3d342e7f0b6cad4
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-11 00:05:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,22 @@
-school = School.new("Bayside High School")
-
-school.roster
-
-school.add_student("Zach Morris", 9)
-school.roster
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, level)
+    # this sort of means #=> a || a = b ... or #=> x || x = y
+    # it's freak'n weird
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end 
