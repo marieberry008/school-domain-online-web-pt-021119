d38ae790bdd7577da890664e3b4791f5f9e22737
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-11 00:11:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,14 +12,6 @@ class School
     roster[level] ||= []
     roster[level] << student
   end 
-  
-    
-  def grade(level)
-    roster.detect do |x, y| 
-      if x == level
-        return y 
-      end 
-    end 
     
   def sort 
   nu_hash = {}
