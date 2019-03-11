f23f7e250c81980b9666798be639cccaff2eac44
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-11 00:16:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,20 +10,21 @@ class School
     roster[level] ||= []
     roster[level] << student
   end 
-    
+  
   def grade(level)
-  roster.detect do |x, y| 
-    if x == level
-    return y 
-    end 
+    roster.detect do |x, y| 
+      if x == level
+      return y 
+      end 
   end 
-    
-  def sort 
+  end 
+
+def sort 
   nu_hash = {}
   roster.each do |x, y| 
     nu_hash[x] = y.sort 
-    end 
-  nu_hash
   end 
-  
+  nu_hash
 end 
+  
+end
